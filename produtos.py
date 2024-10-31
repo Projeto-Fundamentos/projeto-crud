4c3f31b0e53ba4369fa2c8f91b3c8165cadcad85
Victor Simas <victorsimasdev@gmail.com>
2024-10-31 10:13:43 -0300

    feat: Adicionado menu para testes

--- produtos.py
+++ produtos.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pyautogui
 
 products_file = os.path.join(os.path.dirname(__file__), "products.json")
 
@@ -7,5 +8,60 @@ if not os.path.exists(products_file):
     with open(products_file, 'w') as file:
         json.dump({}, file, indent=4)
 
-def load_file():
-    
+class Product:
+    def __init__(self, product_id, name, description, value, available):
+        self.product_id = product_id
+        self.name = name
+        self.description = description
+        self.value = value
+        self.available = available
+
+    @classmethod
+    def menu(cls):
+        while True:
+            option = pyautogui.prompt(
+                text= "Escolha uma opção:\n1. Adicionar Produto\n0. Sair",
+                title= "Menu de Produtos"
+            )
+            match(option):
+                case "1":
+                    product_id = pyautogui.prompt("ID do produto: ")
+                    name = pyautogui.prompt("Nome do produto: ")
+                    description = pyautogui.prompt("Descrição do produto: ")
+                    value = pyautogui.prompt("Valor do produto: ")
+                    available = pyautogui.prompt("Produto disponível? (True/False)")
+
+                    try:
+                        value = float(value)
+                        available = available.lower() == 'true'
+                    except ValueError:
+                        pyautogui.alert("Entrada inválida para valor ou disponibilidade!")
+                        continue
+                    cls.add_product(product_id, name, description, value, available)
+                case "0":
+                    pyautogui.alert("Encerrando o programa.")
+                    break
+                case _:
+                    pyautogui.alert("Opção inválida. Tente novamente.")
+                    continue
+
+    @staticmethod
+    def load_file():
+        with open(products_file, 'r') as file:
+            return json.load(file)
+        
+    @classmethod    
+    def add_product(cls, product_id, name, description, value, available):
+        data = cls.load_file()
+        data[product_id] = {
+            'name': name,
+            'description': description,
+            'value': value,
+            'available': available
+        }
+        with open(products_file, 'w') as file:
+            json.dump(data, file, indent=4)
+        pyautogui.alert(f"Produto '{name}' adicionado com sucesso!")
+
+if __name__ == "__main__":
+    Product.menu()
