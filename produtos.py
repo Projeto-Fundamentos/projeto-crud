33affce81875b4d0c7abdeee0b4bc32bdaf92727
Victor Simas <victorsimasdev@gmail.com>
2024-11-06 13:45:56 -0300

    fix: Removido endpoint do arquivo products.py

--- produtos.py
+++ produtos.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from fastapi import FastAPI, HTTPException
+from fastapi import HTTPException
 from pydantic import BaseModel
 
 products_file = os.path.join(os.path.dirname(__file__), "products.json")
@@ -9,9 +9,6 @@ products_file = os.path.join(os.path.dirname(__file__), "products.json")
 if not os.path.exists(products_file):
     with open(products_file, 'w') as file:
         json.dump({}, file, indent=4)
-
-app = FastAPI()
-
 class ProductModel(BaseModel):
     name: str
     description: str
@@ -65,19 +62,3 @@ class Product:
             Product.save_file(data)
         else:
             raise HTTPException(status_code=404, detail="Campo inválido para atualizar")
-
-@app.post("/adicionar_produto/")
-def add_product(product: ProductModel):
-    product_id = Product.generate_id()
-    Product.add_product(product_id, product.name, product.description, product.value, product.available)
-    return {"message": f"Produto '{product.name}' adicionado com sucesso!", "product_id": product_id}
-
-@app.put("/atualizar_produto/{product_id}")
-def update_product(product_id: str, update_data: ProductUpdateModel):
-    Product.update_product(product_id, update_data.field, update_data.value)
-    return {"message": f"Produto '{product_id}' atualizado com sucesso!"}
-
-@app.get("/listar_produtos/")
-def get_products():
-    data = Product.load_file()
-    return data
