1a3bee4ec8ae8e3a41f5a920258ff003fffb8ec0
marina <marina.resende.lira@avanade.com>
2024-11-12 06:50:54 -0300

    commit ajuste

--- crudServicos/crud_servicos.py
+++ crudServicos/crud_servicos.py
@@ -1,86 +1,125 @@
 import json
 import os
-import uuid
 
 services = os.path.join(os.path.dirname(__file__), 'services.json')
 
 def carregar_servicos():
     if not os.path.exists(services):
         with open(services, 'w') as f:
-            json.dump([], f, indent=4)
+            json.dump({}, f, indent=4)
     with open(services, 'r') as f:
         return json.load(f)
 
+def gerar_id():
+    data = carregar_servicos()
+    if data:
+        maxId = max(int(servicoId) for servicoId in data.keys())
+        return str(maxId+1)
+    return "1"
+
+class Servico:
+    def __init__(self, nome, descricao, duracao):
+        self.servico_id = gerar_id()
+        self.nome = nome
+        self.descricao = descricao 
+        self.duracao = duracao
 
 #CREATE    
-def adicionar_servicos(id, nome, descricao, duracao):
-    servico = {
-        "id": id,
-        "nome": nome,
-        "descricao": descricao,
-        "duracao": duracao
+def adicionar_servicos(nome, descricao, duracao):
+    lista_servicos = carregar_servicos()
+    novo_servico = Servico(nome, descricao, duracao)
+    lista_servicos[novo_servico.servico_id] = {
+        'nome': nome,
+        'descricao': descricao,
+        'duracao': duracao
     }
-    novo_servico = carregar_servicos()
-    novo_servico.append(servico)
     with open(services, 'w') as f:
-        json.dump(novo_servico, f, indent=4)
+        json.dump(lista_servicos, f, indent=4)
 
+#adicionar_servicos('tosa', 'custa tempo', 10)
+#adicionar_servicos('banho', 'cheiro bom', 5)
+#adicionar_servicos('adestrar', 'pet calmo', 500000)
 
 #READ
-def listar_servicos():
-    servicos = carregar_servicos()
+##ordenado
+def listar_por_nome():
+    lista_servicos = carregar_servicos()
+    lista_por_nome = dict(sorted(lista_servicos.items(), key=lambda servico: servico[1]['nome']))
+    return lista_por_nome
 
-    print('SERVICOS OFERECIDOS:')
-    for servico in servicos:
-        print('--------------------------------------------------------------')
-        print(f'ID: {servico['id']}\tNome: {servico['nome']}\tdescricao: {servico['descricao']}\tduracao: {servico['duracao']}')
-    print('--------------------------------------------------------------')
+
+def listar_por_descricao():
+    lista_servicos = carregar_servicos()
+    lista_por_descricao = dict(sorted(lista_servicos.items(), key=lambda servico: servico[1]['descricao']))
+    return lista_por_descricao
+
+def listar_por_duracao():
+    lista_servicos = carregar_servicos()
+    lista_por_duracao = dict(sorted(lista_servicos.items(), key=lambda servico: int(servico[1]['duracao'])))
+    return lista_por_duracao
+
+def achar_por_id(id):
+    lista_servicos = carregar_servicos()
+    for servico_id, desc in lista_servicos.items():
+        if(servico_id == id):
+            return desc
+
+#print(listar_por_nome())
+
+#print(listar_por_descricao())
+
+#print(listar_por_duracao())
+
+#print(achar_por_id('2'))
+
+
 
 
 #UPDATE
-def atualizar_servico(id, nome, descricao, duracao):
-    servico_atualizado = carregar_servicos()
-    for servico in servico_atualizado:
-        if id == servico['id']:
-            servico['nome'] = nome  
-            servico['descricao'] = descricao            
-            servico['duracao'] = duracao            
-            break
-    with open(services, 'w') as f:
-        json.dump(servico_atualizado, f, indent=4)
-    print('Atualizado')
+def atualizar_servicos(id):
+    lista_servicos = carregar_servicos()
+    for servico_id, desc in lista_servicos.items():
+        if servico_id == id:
+            while True:
+                try:
+                    escolha = int(input('(1) Alterar nome\n(2)Alterar descricao\n(3)Alterar duracao\n(7)Sair\n'))
+                    match escolha:
+                        case 1:
+                            nome = input('Digite o novo nome: ')
+                            lista_servicos[servico_id]['nome'] = nome
+                        case 2:
+                            cor = input('Digite a nova descricao: ')
+                            lista_servicos[servico_id]['descricao'] = desc
+                        case 3:
+                            duracao = input('Digite a nova duracao: ')
+                            lista_servicos[servico_id]['duracao'] = duracao
+                        case 7:
+                            break
+                        case __:
+                            print('Opção invalida.\nDigite uma opção valida: ')
+                except ValueError:
+                    print('Entrada invalida.\nDigite uma opção valida: ')
+        print('Campo atualizado com sucesso!')
+        with open(services, 'w') as f:
+            json.dump(lista_servicos, f, indent=4)
+
+
+#atualizar_servicos('2')
+#print(listar_por_nome())
 
 #DELETE
-def excluir_servico(id):
-    servico_excluido = carregar_servicos()
-    for servico in servico_excluido:
-        if id == servico['id']:
-            print(id, servico['id'], servico['nome'])
-            servico_excluido.remove(servico)
-            break
-    with open(services, 'w') as f:
-        json.dump(servico_excluido, f, indent=4)
-    print('servico excluido!')
+def remover_servicos(id):
+    lista_servico = carregar_servicos()
+    servico = False
+    for servico_id, desc in lista_servico.items():
+        if id == servico_id:
+            servico = True
+            del lista_servico[servico_id]
+            with open(services, 'w') as f:
+                json.dump(lista_servico, f, indent=4)
+            print('Servico removido!')
+            break 
+    if servico == False:
+        print('Servico não encontrado')
 
-
-
-
-
-
-# #TESTE
-# adicionar_servicos('1','banho', 'blabla','10')
-# adicionar_servicos('2','banho e tosa', 'kakaka','30')
-# adicionar_servicos('3','cortar unha', 'bugbug','3')
-
-# listar_servicos()
-
-
-# atualizar_servico('1', 'banho premium plus', '500 dolares', '300')
-# print('-------------Atualizados---------------')
-# listar_servicos()
-
-# excluir_servico('1')
-
-# print('-------------Atualizado---------------')
-# listar_servicos()
-
+#remover_servicos('2')
