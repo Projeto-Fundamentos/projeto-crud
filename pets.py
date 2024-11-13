cbc30e9bf31e2814c095225d6fd6b53da6e1a317
Juliobzr <49211890+Juliobzr@users.noreply.github.com>
2024-11-13 00:52:21 -0300

    adicionado tratamento de erro

--- pets.py
+++ pets.py
@@ -28,6 +28,7 @@ class Pet:
         self.adotado = 0
 
 #CREATE
+#QUANDO CHAMAR ESSA FUNCTION IDADE TEM QUE SER UM INTEIRO
 def adicionar_pet(nome, idade, raca, cor):
     lista_pet = carregar_pets()
     novo_pet = Pet(nome, idade, raca, cor)
@@ -45,68 +46,96 @@ def adicionar_pet(nome, idade, raca, cor):
 #READ
 def listar_por_idade():
     lista_pets = carregar_pets()
-    lista_por_idade = dict(sorted(lista_pets.items(), key=lambda pet: int(pet[1]['idade'])))
-    return lista_por_idade
+    if not lista_pets:
+        print('Nem um pet cadastrado.')
+    else:
+        lista_por_idade = dict(sorted(lista_pets.items(), key=lambda pet: int(pet[1]['idade'])))
+        return lista_por_idade
 
 def listar_por_raca():
     lista_pets = carregar_pets()
-    lista_por_raca = dict(sorted(lista_pets.items(), key = lambda pet: pet[1]['raca']))
-    return lista_por_raca
+    if not lista_pets:
+        print('Nem um pet cadastrado.')
+    else:
+        lista_por_raca = dict(sorted(lista_pets.items(), key = lambda pet: pet[1]['raca']))
+        return lista_por_raca
 
 def achar_por_id(id):
     lista_pets = carregar_pets()
-    for pet_id, desc in lista_pets.items():
-        if(pet_id == id):
-            return desc
+    if not lista_pets:
+        print('Nem um pet cadastrado.')
+    else:
+        pet = False
+        for pet_id, desc in lista_pets.items():
+            if(pet_id == id):
+                pet = True
+                return desc
+        if pet == False:
+            print('Id não encontrada.')
+        with open(pets, 'w') as f:
+            json.dump(lista_pets, f, indent=4)
 
 #UPDATE
 def atualizar_pet(id):
     lista_pets = carregar_pets()
-    for pet_id, desc in lista_pets.items():
-        if pet_id == id:
-            while True:
-                try:
-                    escolha = int(input('(1) Alterar nome\n(2)Alterar cor\n(3)Alterar raça\n(4)Alterar idade\n(5)Adicionar observação\n(6)Alterar adoção\n(7)Sair\n'))
-                    match escolha:
-                        case 1:
-                            nome = input('Digite o novo nome: ')
-                            lista_pets[pet_id]['nome'] = nome
-                        case 2:
-                            cor = input('Digite a nova cor: ')
-                            lista_pets[pet_id]['cor'] = cor
-                        case 3:
-                            raca = input('Digite a nova raca: ')
-                            lista_pets[pet_id]['raca'] = raca
-                        case 4:
-                            idade = input('Digite a nova idade: ')
-                            lista_pets[pet_id]['idade'] = idade
-                        case 5:
-                            obs = input('Digite a observação: ')
-                            lista_pets[pet_id]['obs'] = obs
-                        case 6:
-                            adotado = input('Pet adotado?\n(1)Sim\n(0)Não:\n ')
-                            lista_pets[pet_id]['adotado'] = adotado
-                        case 7:
-                            break
-                        case __:
-                            print('Opção invalida.\nDigite uma opção valida: ')
-                except ValueError:
-                    print('Entrada invalida.\nDigite uma opção valida: ')
-        print('Campo atualizado com sucesso!')
-        with open(pets, 'w') as f:
-            json.dump(lista_pets, f, indent=4)
+    if not lista_pets:
+        print('Nem um pet cadastrado.')
+    else:
+        pet = False
+        for pet_id, desc in lista_pets.items():
+            if pet_id == id:
+                pet = True
+                while True:
+                    try:
+                        escolha = int(input('(1) Alterar nome\n(2) Alterar cor\n(3) Alterar raça\n(4) Alterar idade\n(5) Adicionar observação\n(6) Alterar adoção\n(7) Sair\n'))
+                        match escolha:
+                            case 1:
+                                nome = input('Digite o novo nome: ')
+                                lista_pets[pet_id]['nome'] = nome
+                            case 2:
+                                cor = input('Digite a nova cor: ')
+                                lista_pets[pet_id]['cor'] = cor
+                            case 3:
+                                raca = input('Digite a nova raca: ')
+                                lista_pets[pet_id]['raca'] = raca
+                            case 4:
+                                try:
+                                    idade = int(input('Digite a nova idade: '))
+                                    lista_pets[pet_id]['idade'] = idade
+                                except ValueError:
+                                    print('Entrada invalida, digite um numero inteiro.')
+                            case 5:
+                                obs = input('Digite a observação: ')
+                                lista_pets[pet_id]['obs'] = obs
+                            case 6:
+                                adotado = input('Pet adotado?\n(1)Sim\n(0)Não:\n ')
+                                lista_pets[pet_id]['adotado'] = adotado
+                            case 7:
+                                break
+                            case __:
+                                print('Opção invalida.\nDigite uma opção valida: ')
+                    except ValueError:
+                        print('Entrada invalida.\nDigite uma opção valida: ')
+        if pet == True:
+            with open(pets, 'w') as f:
+                json.dump(lista_pets, f, indent=4)
+        else:
+            print('Id não encontrado.')
 
 #DELETE
 def remover_pet(id):
     lista_pets = carregar_pets()
-    pet = False
-    for pet_id, desc in lista_pets.items():
-        if id == pet_id:
-            pet = True
-            del lista_pets[pet_id]
-            with open(pets, 'w') as f:
-                json.dump(lista_pets, f, indent=4)
-            print('Pet removido!')
-            break 
-    if pet == False:
-        print('Pet não encontrado')
+    if not lista_pets:
+        print('Nem um pet cadastrado.')
+    else:
+        pet = False
+        for pet_id, desc in lista_pets.items():
+            if id == pet_id:
+                pet = True
+                del lista_pets[pet_id]
+                with open(pets, 'w') as f:
+                    json.dump(lista_pets, f, indent=4)
+                print('Pet removido!')
+                break 
+        if pet == False:
+            print('Pet não encontrado')
