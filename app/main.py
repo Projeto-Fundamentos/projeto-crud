0694b86692cc78a463880a6cca88966f0ecbd4e2
Victor <victorsimasazevedo@gmail.com>
2024-11-12 23:33:57 -0300

    feat: Adicionadas rotas de pets

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routes.products import router as products_router
+from app.routes.pets import router as pets_router
 
 app = FastAPI()
 
 app.include_router(products_router)
+app.include_router(pets_router)
