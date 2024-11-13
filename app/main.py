20ac8d5d77781494ae12ca95640bad26ef2e951f
Victor <victorsimasazevedo@gmail.com>
2024-11-12 23:56:58 -0300

    feat: Adicionado router do funcionarios e movido clientes e funcionarios para pasta legacy

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from app.routes.products import router as products_router
 from app.routes.pets import router as pets_router
 from app.routes.clientes import router as clientes_router
+from app.routes.funcionarios import router as funcionarios_router
 
 app = FastAPI()
 
 app.include_router(products_router)
 app.include_router(pets_router)
 app.include_router(clientes_router)
+app.include_router(funcionarios_router)
