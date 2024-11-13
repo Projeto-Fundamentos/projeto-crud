8febbf7555362b8bcacc8ea8b5341ffb8445d1ea
Victor <victorsimasazevedo@gmail.com>
2024-11-13 00:09:49 -0300

    feat: Adicionado router de servicos na main

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.routes.products import router as products_router
 from app.routes.pets import router as pets_router
 from app.routes.clientes import router as clientes_router
 from app.routes.funcionarios import router as funcionarios_router
+from app.routes.servicos import router as servicos_router
 
 app = FastAPI()
 
@@ -10,3 +11,4 @@ app.include_router(products_router)
 app.include_router(pets_router)
 app.include_router(clientes_router)
 app.include_router(funcionarios_router)
+app.include_router(servicos_router)
