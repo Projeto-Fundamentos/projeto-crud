87bcd43dc8ed3db399a1ca8309cf81dda2e9bb4e
Victor <victorsimasazevedo@gmail.com>
2024-11-12 23:46:24 -0300

    feat: Adicionado router do clientes no main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.routes.products import router as products_router
 from app.routes.pets import router as pets_router
+from app.routes.clientes import router as clientes_router
 
 app = FastAPI()
 
 app.include_router(products_router)
 app.include_router(pets_router)
+app.include_router(clientes_router)
